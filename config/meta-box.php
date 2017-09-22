e06ff3a2558e2fb619267e4b3a90453e72d08b45
hellofromtonya <hellofromtonya@knowthecode.io>
2017-09-22 03:12:05 -0500

    enhance: config
    1. Move the runtime configuration parameters into a config file.
    2. Updated the meta box boilerplate to use the config parameters.
    3. Moved the "okay to save custom fields" logic to a function.

---
+++ config/meta-box.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Runtime configuration parameters.
+ *
+ * @package     KnowTheCode\MetaBoxBasics
+ * @since       1.0.0
+ * @author      hellofromTonya
+ * @link        https://KnowTheCode.io
+ * @license     GNU-2.0+
+ */
+
+namespace KnowTheCode\MetaBoxBasics;
+
+return array(
+	'add_meta_box' => array(
+		// The key that holds all of the custom fields
+		// in the $_POST[ $custom_field_key ]
+		'custom_field_key'  => '',
+		// Meta box ID (used in the 'id' attribute for the meta box)
+		'id'            => '',
+		// Title of the meta box
+		'title'         => '',
+		// The screen or screens on which to show the box
+		// such as a post type, link, comment, etc.
+		'screen'        => null,
+		// (Optional) The context within the screen where this will display.
+		// Choices: normal, side, or advanced (default).
+		'context'       => 'advanced',
+		// (Optional) Sets the priority of when the meta box will render.
+		// Choices: high, low, or default (which is the default).
+		'priority'      => 'default',
+		// (Optional) You can send arguments to your render callback.
+		// Send as an array of arguments.
+		'callback_args' => array(),
+	),
+	'nonce'        => array(
+		'nonce_action' => '',
+		'nonce_name'   => '',
+	),
+	'metadata'     => array(
+		// fill in the metadata here.
+		// 'meta_key' => 'default value',
+	),
+	'view'         => '',
+);
--- src/meta-box.php
+++ src/meta-box.php
@@ -24,15 +24,17 @@ add_action( 'admin_menu', __NAMESPACE__ . '\register_meta_box' );
 function register_meta_box() {
 	$config = getConfig( 'add_meta_box' );
 
-	add_meta_box(
-		$config['id'],
-		$config['title'],
-		$config['callback'],
-		$config['screen'],
-		$config['context'],
-		$config['priority'],
-		$config['callback_args']
-	);
+	foreach( (array) $config['screen'] as $screen ) {
+		add_meta_box(
+			$config['id'],
+			$config['title'],
+			__NAMESPACE__ . '\render_meta_box',
+			$screen,
+			$config['context'],
+			$config['priority'],
+			$config['callback_args']
+		);
+	}
 }
 
 /**
@@ -73,56 +75,79 @@ add_action( 'save_post', __NAMESPACE__ . '\save_meta_box', 10, 2 );
 function save_meta_box( $post_id, $post ) {
 	$config = getConfig();
 
-	// If the nonce doesn't match, bail out.
-	if ( ! wp_verify_nonce(
-		$_POST[ $config['nonce']['nonce_name'] ],
-		$config['nonce']['nonce_action'] )
+	if ( ! is_ok_to_save_custom_field(
+		$config['nonce']['nonce_name'],
+		$config['nonce']['nonce_action'],
+		$config['post_key'],
+		$post->post_type )
 	) {
 		return;
 	}
 
-	// prepare the data
+	// Merge the metadata.
+	$data = wp_parse_args(
+		$_POST[ $config['post_key'] ],
+		$config['metadata']
+	);
 
-	// If there's data to save/update, do it here.
-	// update_post_meta($post_id, $meta_key, $data);
-
-	// else, delete it.
-	// delete_post_meta($post_id, $meta_key);
-
+	// Loop through the custom fields and update the `wp_postmeta` database.
+	foreach ( $data as $meta_key => $value ) {
+		if ( $value ) {
+			update_post_meta( $post_id, $meta_key, strip_tags( $value ) );
+		} else {
+			delete_post_meta( $post_id, $meta_key );
+		}
+	}
 }
 
-function getConfig( $key_to_get = '' ) {
-	$config = array(
-		'add_meta_box' => array(
-			// Meta box ID (used in the 'id' attribute for the meta box)
-			'id'            => '',
-			// Title of the meta box
-			'title'         => '',
-			// callback
-			'callback'      => '\\KnowTheCode\MetaBoxBasics\render_meta_box',
-			// The screen or screens on which to show the box
-			// such as a post type, link, comment, etc.
-			'screen'        => null,
-			// (Optional) The context within the screen where this will display.
-			// Choices: normal, side, or advanced (default).
-			'context'       => 'advanced',
-			// (Optional) Sets the priority of when the meta box will render.
-			// Choices: high, low, or default (which is the default).
-			'priority'      => 'default',
-			// (Optional) You can send arguments to your render callback.
-			// Send as an array of arguments.
-			'callback_args' => array(),
-		),
-		'nonce'        => array(
-			'nonce_action' => '',
-			'nonce_name'   => '',
-		),
-		'metadata'     => array(
-			// fill in the metadata here.
-			// 'meta_key' => 'default value',
-		),
-		'view'         => '',
+/**
+ * Checks if it's okay to save custom field(s).
+ *
+ * @since 1.0.0
+ *
+ * @param string $nonce_name Nounce name
+ * @param string $nonce_action Nonce action
+ * @param string $custom_field_key Custom field key $_POST[ $custom_field_key ]
+ * @param string $post_type Post type for this post
+ *
+ * @return bool
+ */
+function is_ok_to_save_custom_field( $nonce_name, $nonce_action, $custom_field_key, $post_type ) {
+	// If the nonce doesn't match, return false.
+	if ( ! wp_verify_nonce( $_POST[ $nonce_name ], $nonce_action ) ) {
+		return false;
+	}
+
+	// If there's no data, return false.
+	if ( ! isset( $_POST[ $custom_field_key ] ) ) {
+		return;
+	}
+
+	// When doing an autosave, Ajax, future post, or revision,
+	// don't save the custom fields.
+	return ! (
+		( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) ||
+		( defined( 'DOING_AJAX' ) && DOING_AJAX ) ||
+		( defined( 'DOING_CRON' ) && DOING_CRON ) ||
+		( 'revision' === $post_type )
 	);
+}
+
+/**
+ * Get the runtime configuration parameters.
+ *
+ * @since 1.0.0
+ *
+ * @param string $key_to_get (Optional) Key to get from the configuration.
+ *
+ * @return mixed
+ */
+function getConfig( $key_to_get = '' ) {
+	static $config;
+
+	if ( ! $config ) {
+		$config = require METABOXBASICS_DIR . 'config/meta-box.php';
+	}
 
 	if ( ! $key_to_get || ! array_key_exists( $key_to_get, $config ) ) {
 		return $config;
