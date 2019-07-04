7fe31a15262e6ad8964b2135b77bcb9e7b0e98e3
Tonya Mork <hellofromtonya@knowthecode.io>
2019-07-04 14:35:40 -0500

    Fixed meta_key_id in the subtitle view.

--- src/views/subtitle.php
+++ src/views/subtitle.php
@@ -1,11 +1,11 @@
 <p>
 	<label for="subtitle"><?php _e( 'Subtitle', 'mbreusable' ); ?></label>
-	<input class="large-text" type="text" name="<?php echo $meta_box_key; ?>[subtitle]" value="<?php esc_attr_e( $custom_fields['subtitle'] ); ?>">
+	<input class="large-text" type="text" name="<?php echo $meta_box_id; ?>[subtitle]" value="<?php esc_attr_e( $custom_fields['subtitle'] ); ?>">
 	<span class="description"><?php _e( 'Enter the subtitle for this piece of content.', 'mbreusable' ); ?></span>
 </p>
 
 <p>
-    <input type="checkbox" value="1" name="<?php echo $meta_box_key; ?>[show_subtitle]" <?php checked( $custom_fields['show_subtitle'], 1 ); ?>
+    <input type="checkbox" value="1" name="<?php echo $meta_box_id; ?>[show_subtitle]" <?php checked( $custom_fields['show_subtitle'], 1 ); ?>
     <label for="subtitle"><?php _e( 'Show Subtitle?', 'mbreusable' ); ?></label>
     <div><span class="description"><?php _e( 'Check if you want to show the subtitle for this article.', 'mbreusable' ); ?></span></div>
 </p>
